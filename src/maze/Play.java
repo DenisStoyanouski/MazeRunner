cdf2cee418a0cf9b07d50f08cd78c497f6414502
Denis Stoyanouski <vandenst@tut.by>
2022-09-20 21:10:01 +0300

    added method fillGraphMatrix

--- src/maze/Play.java
+++ src/maze/Play.java
@@ -1,17 +1,22 @@
 package maze;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Play {
     int lines;
     int columns;
-    String [][] maze = new String[lines][columns];
+    String [][] maze;
+    int [][] graphMatrix;
+
     final
     public void go() {
         getGridSize();
-        fillMaze();
-        displayMaze();
+        fillGraphMatrix();
+        displayGraphMatrix();
+        /*fillMaze();
+        displayMaze();*/
     }
 
     private String getInput() {
@@ -20,13 +25,33 @@ public class Play {
     }
 
     private void getGridSize() {
+        System.out.println("Please, enter the size of a maze");
         String[] gridSize = getInput().trim().split("\\s+");
-        lines = Integer.parseInt(gridSize[0]);
-        columns = Integer.parseInt(gridSize[0]);
+        try {
+            lines = Integer.parseInt(gridSize[0]);
+            columns = Integer.parseInt(gridSize[1]);
+        } catch (Exception e) {
+            System.out.println("Please, enter two numbers");
+            getGridSize();
+        }
+        maze = new String[lines][columns];
+        graphMatrix = new int[lines][columns];
+    }
+
+    private void fillGraphMatrix() {
+        for (int i = 0; i < graphMatrix.length; i++) {
+            for (int j = 0; j < graphMatrix[0].length; j++) {
+                graphMatrix[i][j] = getWeight();
+            }
+        }
+    }
+
+    private void displayGraphMatrix() {
+        System.out.println(Arrays.deepToString(graphMatrix));
     }
 
 
-    private void fillMaze() {
+    /*private void fillMaze() {
         //fill blocks and empty cells
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
@@ -59,7 +84,7 @@ public class Play {
         int line = getNumberOfLine();
         maze[line][9] = "  ";
         maze[line][8] = "  ";
-    }
+    }*/
     
     private void displayMaze() {
         for (String[] line : maze) {
@@ -70,9 +95,9 @@ public class Play {
         }
     }
 
-    private int getNumberOfLine() {
+    private int getWeight() {
         Random random = new Random();
-        return random.nextInt(8) + 1;
+        return random.nextInt(20);
     }
 
     private int getAmountOfCell() {
