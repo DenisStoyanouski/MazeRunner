d3ceea2636131c349a02289daf1f9f33a49d29ad
Denis Stoyanouski <vandenst@tut.by>
2022-09-24 20:32:58 +0300

    changed method fillMaze

--- src/maze/Play.java
+++ src/maze/Play.java
@@ -33,15 +33,16 @@ public class Play {
     }
 
     private void fillEdgesMatrix() {
-
-        for (int i = 0; i < lines; i++) {
-            for (int j = 0; j < columns; j++) {
+        int nodeLines = lines - 2;
+        int nodeColumns = columns - 2;
+        for (int i = 0; i < nodeLines; i++) {
+            for (int j = 0; j < nodeColumns; j++) {
                 ArrayList<List<Integer>> edgesOfGraph = new ArrayList<>();
-                if (j != columns - 1) {
-                    edgesOfGraph.add(List.of(j + i * columns + 1, getWeight()));
+                if (j != nodeColumns - 1) {
+                    edgesOfGraph.add(List.of(j + i * nodeColumns + 1, getWeight()));
                 }
-                if (i != lines - 1) {
-                    edgesOfGraph.add(List.of(j + (i + 1) * columns, getWeight()));
+                if (i != nodeLines - 1) {
+                    edgesOfGraph.add(List.of(j + (i + 1) * nodeColumns, getWeight()));
                 }
                 edges.add(edgesOfGraph);
             }
@@ -50,7 +51,6 @@ public class Play {
     }
 
     private void createSpanningTree(ArrayList<ArrayList<List<Integer>>> edges) {
-        System.out.println("This is a spanning tree:");
         //sort edges by weights
         for (var edge : edges) {
             edge.sort((edge1, edge2) -> edge1.get(1).compareTo(edge2.get(1)));
@@ -69,23 +69,21 @@ public class Play {
     }
 
     private void fillMaze() {
-        //fill blocks and empty cells
+        //fill all cells with blocks
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
-                if (edges.get(i * columns + j).size() < 2) {
                     maze[i][j] = "\u2588".repeat(2);
-                } else {
+            }
+        }
+        //fill cells with maze according to spanning tree
+        for (int i = 1; i < maze.length - 1; i++) {
+            for (int j = 1; j < maze[0].length - 1; j++) {
+                if (edges.get((i - 1) * (columns - 2) + (j - 1)).size() > 1) {
                     maze[i][j] = " ".repeat(2);
                 }
             }
         }
-        for (int i = 0; i < maze.length; i++) {
-            for (int j = 0; j < maze[0].length; j++) {
-                if (i == 0 || i == maze.length - 1 || j == 0 || j == maze[0].length - 1) {
-                    maze[i][j] = "\u2588".repeat(2);
-                }
-            }
-        }
+
         getEnter();
         getEntrance();
         displayMaze();
@@ -93,17 +91,16 @@ public class Play {
     }
 
     private void getEnter() {
-        int line = getRandomLine();
+        int line;
+        line = getRandomLine();
         if ("  ".equals(maze[line][1])) {
             maze[line][0] = " ".repeat(2);
-        } else {
-            getEnter();
-        }
-
+        } else getEnter();
     }
 
     private void getEntrance() {
-        int line = getRandomLine();
+        int line;
+        line = getRandomLine();
         if ("  ".equals(maze[line][columns - 2])) {
             maze[line][columns - 1] = " ".repeat(2);
         } else {
@@ -131,6 +128,5 @@ public class Play {
         Random random = new Random();
         return random.nextInt(lines - 1) + 1;
     }
-
 
 }
