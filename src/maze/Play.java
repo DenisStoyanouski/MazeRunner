579701e782a6932973f60af6d7d2a8e24acb44e8
Denis Stoyanouski <vandenst@tut.by>
2022-09-24 19:52:36 +0300

    added method getEnter

--- src/maze/Play.java
+++ src/maze/Play.java
@@ -7,18 +7,15 @@ public class Play {
     int lines;
     int columns;
     String [][] maze;
-    int [][] graphMatrix;
+
+    ArrayList<ArrayList<List<Integer>>> edges = new ArrayList<>();
 
     final
     public void go() {
         getGridSize();
         fillEdgesMatrix();
-
-
-        /*fillGraphMatrix();
-        displayGraphMatrix();*/
-        /*fillMaze();
-        displayMaze();*/
+        fillMaze();
+        displayMaze();
     }
 
     private String getInput() {
@@ -37,51 +34,48 @@ public class Play {
             getGridSize();
         }
         maze = new String[lines][columns];
-        graphMatrix = new int[lines][columns];
     }
-
-    /*private void fillGraphMatrix() {
-        for (int i = 0; i < graphMatrix.length; i++) {
-            for (int j = 0; j < graphMatrix[0].length; j++) {
-            }
-        }
-    }*/
 
     private void fillEdgesMatrix() {
 
-        ArrayList<ArrayList<List<Integer>>> edges = new ArrayList<>();
         for (int i = 0; i < lines; i++) {
             for (int j = 0; j < columns; j++) {
                 ArrayList<List<Integer>> edgesOfGraph = new ArrayList<>();
                 if (j != columns - 1) {
-                    edgesOfGraph.add(List.of(j + (i * 10) + 1, getWeight()));
+                    edgesOfGraph.add(List.of(j + i * columns + 1, getWeight()));
                 }
                 if (i != lines - 1) {
-                    edgesOfGraph.add(List.of(j + (i + 1) * lines, getWeight()));
+                    edgesOfGraph.add(List.of(j + (i + 1) * columns, getWeight()));
                 }
                 edges.add(edgesOfGraph);
             }
         }
         displayEdgesMatrix(edges);
-        createSpanningTree(edges);
     }
 
     private void displayEdgesMatrix(ArrayList<ArrayList<List<Integer>>> edges) {
         System.out.println("This is EdgesMatrix");
-
+        for (var node : edges) {
+            System.out.println("Node " + edges.indexOf(node) + "--> " + node.toString());
+        }
         createSpanningTree(edges);
     }
 
     private void createSpanningTree(ArrayList<ArrayList<List<Integer>>> edges) {
         System.out.println("This is a spanning tree:");
+        //sort edges by weights
         for (var edge : edges) {
             edge.sort((edge1, edge2) -> edge1.get(1).compareTo(edge2.get(1)));
         }
-
+        //leave only edge with minimal weight
         for (var edge : edges) {
             if (edge.size() > 1) {
                 edge.remove(1);
             }
+        }
+        //add edges to another graphs
+        for (var edge : edges) {
+            edges.get(edge.get(0).get(0)).add(edge.get(0));
         }
 
         displaySpanningTree(edges);
@@ -93,46 +87,51 @@ public class Play {
         }
     }
 
-    private void displayGraphMatrix() {
-        System.out.println(Arrays.deepToString(graphMatrix));
-    }
 
-
-    /*private void fillMaze() {
+    private void fillMaze() {
         //fill blocks and empty cells
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
-                if (i == 0 || i == 9 || j % 2 == 0 || j == 9) {
+                if (edges.get(i * columns + j).size() < 2) {
                     maze[i][j] = "\u2588".repeat(2);
                 } else {
                     maze[i][j] = " ".repeat(2);
                 }
             }
         }
-        enterEntrance();
-        enterExit();
-
-        for (int k = 2; k < 9; k +=2 ) {
-            int amount = getAmountOfCell();
-            int line = getNumberOfLine();
-            while (!"  ".equals(maze[line][k])) {
-                if (!"  ".equals(maze[line - 1][k - 2]) && !"  ".equals(maze[line + 1][k - 2])) {
-                    maze[line][k] = " ".repeat(2);
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                if (i == 0 || i == maze.length - 1 || j == 0 || j == maze[0].length - 1) {
+                    maze[i][j] = "\u2588".repeat(2);
                 }
             }
         }
+        getEnter();
+        getEntrance();
+
     }
 
-    private void enterEntrance() {
-        maze[getNumberOfLine()][0] = "  ";
+    private void getEnter() {
+        int line = getRandomLine();
+        if ("  ".equals(maze[line][1])) {
+            maze[line][0] = " ".repeat(2);
+        } else {
+            getEnter();
+        }
+
     }
 
-    private void enterExit() {
-        int line = getNumberOfLine();
-        maze[line][9] = "  ";
-        maze[line][8] = "  ";
-    }*/
-    
+    private void getEntrance() {
+        int line = getRandomLine();
+        if ("  ".equals(maze[line][columns - 2])) {
+            maze[line][columns - 1] = " ".repeat(2);
+        } else {
+            getEntrance();
+        }
+
+    }
+
+
     private void displayMaze() {
         for (String[] line : maze) {
             for (String cell : line) {
@@ -144,12 +143,12 @@ public class Play {
 
     private int getWeight() {
         Random random = new Random();
-        return random.nextInt(20);
+        return random.nextInt(100);
     }
 
-    private int getAmountOfCell() {
+    private int getRandomLine() {
         Random random = new Random();
-        return random.nextInt(2) + 1;
+        return random.nextInt(lines - 1) + 1;
     }
 
 
